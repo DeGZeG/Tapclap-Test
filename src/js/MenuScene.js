60c2a8abc9aa13387890cd599560456405a2efc3
Dmitrii Gribovskii <degzeg@gmail.com>
2020-11-05 01:35:46 +0500

    fix images

--- src/js/MenuScene.js
+++ src/js/MenuScene.js
@@ -19,19 +19,19 @@ export default class MenuScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('blueTile', '../img/blue.png')
-        this.load.image('greenTile', '../img/green.png')
-        this.load.image('purpleTile', '../img/purple.png')
-        this.load.image('redTile', '../img/red.png')
-        this.load.image('yellowTile', '../img/yellow.png')
-        this.load.image('bombTile', '../img/bomb.png')
+        this.load.image('blueTile', './img/blue.png')
+        this.load.image('greenTile', './img/green.png')
+        this.load.image('purpleTile', './img/purple.png')
+        this.load.image('redTile', './img/red.png')
+        this.load.image('yellowTile', './img/yellow.png')
+        this.load.image('bombTile', './img/bomb.png')
 
-        this.load.image('field', '../img/field.png')
-        this.load.image('bonus', '../img/bonus.png')
-        this.load.image('button', '../img/button.png')
-        this.load.image('button2', '../img/button2.png')
-        this.load.image('pause', '../img/pause.png')
-        this.load.image('scorePanel', '../img/scorePanel.png')
+        this.load.image('field', './img/field.png')
+        this.load.image('bonus', './img/bonus.png')
+        this.load.image('button', './img/button.png')
+        this.load.image('button2', './img/button2.png')
+        this.load.image('pause', './img/pause.png')
+        this.load.image('scorePanel', './img/scorePanel.png')
     }
 
     create() {
